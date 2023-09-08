db629ca10c4aa42826b2a734cde451320016b0ee
Xinyu Bao <34500975+AspadaX@users.noreply.github.com>
2023-09-09 01:14:54 +0800

    Delete i-ching-consultant directory

--- i-ching-consultant/commons/file_handling.py
+++
@@ -1,6 +0,0 @@
-from datetime import datetime
-def save_response_to_file(response, file_name):
-    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-    unique_file_name = f"outputs/{current_time}_{file_name}"
-    with open(unique_file_name, "w") as f:
-        f.write(response)
